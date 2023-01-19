e0eb8beab01a03af7e5c816cfd06f60da62196bd
kittypitty <samutic40@gmail.com>
2023-01-19 13:53:09 +0300

    fix getDeviceId saga

--- src/store/sagas/fetchTasksSaga.ts
+++ src/store/sagas/fetchTasksSaga.ts
@@ -1,16 +1,15 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { FirebaseDatabaseTypes } from '@react-native-firebase/database';
-import { put } from 'redux-saga/effects';
+import { put, select } from 'redux-saga/effects';
 
 import { fetchTasksAC } from '~store/actions/tasksAC';
+import { getDeviceId } from '~store/selectors/appSelector';
 import { database } from '~utils/getDataBaseURL';
 
 import { TaskType } from '../reducers/tasksReducer';
 
 export function* fetchTasksWorker() {
     try {
-        const savedDeviceId: string = yield AsyncStorage.getItem('deviceID');
-        const resultedDeviceId: string = yield JSON.parse(savedDeviceId);
+        const resultedDeviceId: string = yield select(getDeviceId);
         const reference: FirebaseDatabaseTypes.Reference = yield database.ref(`/${resultedDeviceId}/tasks`);
         const snapshot: FirebaseDatabaseTypes.DataSnapshot = yield reference.once('value');
 
--- src/store/sagas/getDeviceIdSaga.ts
+++ src/store/sagas/getDeviceIdSaga.ts
@@ -1,4 +1,3 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getUniqueId } from 'react-native-device-info';
 import { put } from 'redux-saga/effects';
 
@@ -7,7 +6,6 @@ import { saveDeviceID } from '~store/actions/appAC';
 export function* getDeviceIdWorker() {
     try {
         const id: string = yield getUniqueId();
-        yield AsyncStorage.setItem('deviceID', JSON.stringify(id));
         yield put(saveDeviceID(id));
     } catch (error: any) {
         console.warn(error);
